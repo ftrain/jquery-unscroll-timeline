669f3ca58f1cad8dfae6335f5d3ab12896aef436
Paul Ford <ford@ftrain.com>
2011-03-27 15:20:08 -0400

    Update documentation

--- timeline/timeline.js
+++ timeline/timeline.js
@@ -19,6 +19,112 @@
 (function( $ ){
    
     $.fn.place = function( elements ) {
+
+/*
+  This function places arbitrary shapes on a
+  grid with the intent of cramming as many
+  arbitrary shapes on that grid as can fit and
+  make sense.
+  
+  - Take an element
+  - Measure its pixel dimensions X and Y
+  - Divide the width X by the width a single block
+  - Divide the height Y by the height of a single block
+  
+  - Either:
+  
+  - Determine an X_offset based on date/time
+  
+  - Ignore the X_offset and just find some room on the grid
+  
+  - Then scan across, starting at the [X_offset,0] coordinate.
+  
+  
+  STARTING STATE
+  +-----------------+
+  |1|2|3|4|5|6|7|8|9|
+  +------------------
+  | | | | | | | | | |
+  |-----------------|
+  | | | | | | | | | |
+  |-----------------|
+  | | | | | | | | | |
+  |-----------------|
+  | | | | | | | | | |
+  +------------------
+  
+  REQUEST: place a 1x2 element A aligned to position X4
+  +-----------------+
+  |1|2|3|4|5|6|7|8|9|
+  +------------------
+  | | | |A|A| | | | |
+  |-----------------|
+  | | | | | | | | | |
+  |-----------------|
+  | | | | | | | | | |
+  |-----------------|
+  | | | | | | | | | |
+  +------------------
+  
+  REQUEST: place a 2x2 element B aligned to position X5
+  +-----------------+
+  |1|2|3|4|5|6|7|8|9|
+  +------------------
+  | | | |A|A| | | | |
+  |-----------------|
+  | | | | |B|B| | | |
+  |-----------------|
+  | | | | |B|B| | | |
+  |-----------------|
+  | | | | | | | | | |
+  +------------------
+  
+  REQUEST: place a 1x2 element C aligned to position X1
+  +-----------------+
+  |1|2|3|4|5|6|7|8|9|
+  +------------------
+  |C|C| |A|A| | | | |
+  |-----------------|
+  | | | | |B|B| | | |
+  |-----------------|
+  | | | | |B|B| | | |
+  |-----------------|
+  | | | | | | | | | |
+  +------------------
+  
+  REQUEST: place a 3x3 element D aligned to position X2
+  +-----------------+
+  |1|2|3|4|5|6|7|8|9|
+  +------------------
+  |C|C| |A|A| | | | |
+  |-----------------|
+  | |D|D|D|B|B| | | |
+  |-----------------|
+  | |D|D|D|B|B| | | |
+  |-----------------|
+  | |D|D|D| | | | | |
+  +------------------
+  
+  
+  REQUEST: place a 5x1 element E aligned to position X5
+  +-----------------+
+  |1|2|3|4|5|6|7|8|9|
+  +------------------
+  |C|C| |A|A| | | | |
+  |-----------------|
+  | |D|D|D|B|B| | | |
+  |-----------------|
+  | |D|D|D|B|B| | | |
+  |-----------------|
+  | |D|D|D|E|E|E|E|E|
+  +------------------
+  
+  If an element can't be placed for lack of
+  room, it does not appear on the
+  timeline. This is a problem to be solved.
+  
+*/
+
     };
     
     $.fn.timeline = function( elements ) {
